8432d7b80ff2ad2542a3be393e4e06cd24f6734f
Aniket-23 <117341962+Aniket-23@users.noreply.github.com>
2023-06-25 12:01:23 +0530

    added functionality for preloader

--- app.js
+++ app.js
@@ -15,3 +15,9 @@ btn.addEventListener("click", () => {
         container.play();
     }
 });
+
+//preloader
+const preloader = document.querySelector(".preloader");
+window.addEventListener("load", () => {
+    preloader.classList.add("hide-preloader");    
+});
